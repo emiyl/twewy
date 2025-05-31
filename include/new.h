c322bd237a5392fcff98d6ed66632b685efb54a4
emiyl <me@emiyl.com>
2025-05-31 03:42:34 +0200

    add 6 new functions

---
+++ include/new.h
@@ -0,0 +1,13 @@
+#ifndef NEW_C
+#define NEW_C
+#include <types.h>
+
+u32 wait_for_vblank_start();
+void func_020398d0(void);
+void* func_02039918(void);
+s32 func_02039a1c();
+s32 func_02039a88(int index);
+s32 func_02039a9c(int index);
+s32 func_02039ab0(s32 param_1);
+
+#endif // NEW_C
---
+++ src/new.c
@@ -0,0 +1,121 @@
+#include "new.h"
+
+extern void* data_0207df5c;
+extern s32 data_0207df60;
+
+#define DISPCNT_A_ADDR  0x04000000U
+#define IME_A7_OFFSET   0x208U
+#define VCOUNT_OFFSET   0x6U
+
+#define DISPCNT_A (*(volatile u32 *)(DISPCNT_A_ADDR))
+#define IME_A7 (*(volatile u32 *)(DISPCNT_A_ADDR + IME_A7_OFFSET))
+#define VCOUNT (*(volatile u16 *)(DISPCNT_A_ADDR + VCOUNT_OFFSET))
+
+u32 wait_for_vblank_start(void) {
+    s32 savedIME = IME_A7;
+    IME_A7 = &DISPCNT_A;
+    u32 vcount = VCOUNT;
+    
+    do {
+        vcount = (u32)VCOUNT;
+    } while (vcount);
+
+    IME_A7 = savedIME;
+    return vcount;
+}
+
+void func_020398d0(void) {
+    func_02039918();
+    func_0203cc10();
+    func_02038664();
+    func_02039a1c();
+    func_020383bc();
+    func_02038630();
+    func_0203a114();
+    func_0203babc();
+    func_0203a5e4();
+    func_0203aa14();
+    func_02038cb4();
+    func_0203a7fc();
+    func_0204799c();
+    func_02043134();
+    func_02040ac0();
+    wait_for_vblank_start();
+    return;
+}
+
+void* func_02039918(void) {
+    if (data_0207df5c != NULL)
+        return data_0207df5c;
+
+    data_0207df5c = 1;
+    func_02039c3c(0, func_02039ab0(0));
+    func_02039c50(0, func_02039b94(0));
+    func_02039c50(2,0);
+    func_02039c3c(2,0);
+    func_02039c3c(3, func_02039ab0(3));
+    func_02039c50(3, func_02039b94(3));
+    func_02039c3c(4, func_02039ab0(4));
+    func_02039c50(4, func_02039b94(4));
+    func_02039c3c(5, func_02039ab0(5));
+    func_02039c50(5, func_02039b94(5));
+    func_02039c3c(6, func_02039ab0(6));
+    func_02039c50(6, func_02039b94(6));
+    return;
+}
+
+s32 func_02039a1c() {
+    u32 uVar1;
+    u16 uVar2;
+
+    uVar1 = func_2039ab0(2);
+    func_02039c3c(2, uVar1);
+
+    uVar1 = func_2039b94(2);
+    func_02039c50(2, uVar1);
+    
+    if ((data_0207df60 != 0) && (uVar2 = func_02039380(), (uVar2 & 3) != 1)) {
+        func_02039380();
+    }
+
+    func_0203a104(0x200002b);
+    func_0203a10c(0x23e0021);
+
+    return 0x23e0021;
+}
+
+s32 func_02039a88(int index) {
+    return *((s32 *)((index << 2) + 0x27ffdc4));
+}
+
+
+s32 func_02039a9c(int index) {
+    return *((s32 *)((index << 2) + 0x27ffda0));
+}
+
+s32 func_02039ab0(s32 param_1) {
+    u16 uVar1;
+    
+    switch(param_1) {
+        case 0:
+            return 0x23e0000;
+        case 1:
+            return 0;
+        case 2:
+            if ((data_0207df60 != 0) && (uVar1 = func_02039380(), (uVar1 & 3) != 1)) {
+                return 0x2700000;
+            }
+            break;
+        case 3:
+            return 0x2000000;
+        case 4:
+            return 0x27e0780;
+        case 5:
+            return 0x27ff680;
+        case 6:
+            return 0x37f8000;
+        default:
+            return 0;
+    }
+    return 0;
+}
